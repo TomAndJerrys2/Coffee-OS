b5e9e1249347e6da08ba52284c37c189c95843a2
kylev <kylev2005@hotmail.com>
2025-03-17 14:35:50 +0000

    string library

--- clib/structure-lib/string.h
+++ clib/structure-lib/string.h
@@ -5,7 +5,40 @@
 #include "../mem-lib/memory.h"
 
 class String {
+    private:
+        char* baseStr = nullptr;
+        int charCount { 0 };
 
+    public:
+        // Default Constructor
+        String();
+        // Copy-Constructor
+        String(const String& str);
+        // C-String Constructor
+        String(const char* str);
+
+        // Assignment operations
+        String& operator=(const String& str); // for class strings
+        String& operator=(const char* str); // for c-style strings
+        String& operator=(const char c); // for characters
+
+        // Index operations
+        char& operator[](unsigned int position);
+        const char& operator[](unsigned int position) const;
+
+        // Increment/Modify operations
+        String& operator+=(const String& str);
+        String& operator+=(const char* str);
+        String& operator+=(const char c);
+
+        // Class methods
+        int length(String str);
+        void swapString(String& strOne, String& strTwo) const;
+        bool isEmpty(String str);
+        void splitString(String str, int position);
+
+        // String Destructor
+        ~String();
 };
 
 #endif
