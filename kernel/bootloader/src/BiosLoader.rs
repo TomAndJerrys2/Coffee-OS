de29de949abe58a586d197c98135661a1840dd16
kylev <kylev2005@hotmail.com>
2025-04-21 17:26:59 +0100

    bios loader

--- kernel/bootloader/src/BiosLoader.rs
+++ kernel/bootloader/src/BiosLoader.rs
@@ -1,8 +1,32 @@
-pub struct BiosLoader {};
+use std::path::Path // stdlib - needs changing
+use coffee::os::DiskImageBuilder // some module for disk building will make later
+
+pub struct BiosLoader {
+    dskImageBuilder: DiskImageBuilder,
+}
 
 impl BiosLoader {
-    pub fn newImage(kernelPath) -> Self {
-        
+    // Create the disk image when the coffeekernel is processed
+    pub fn new(kernelPath: &Path) -> Self {
+        Self {
+            dskImageBuilder: DiskImageBuilder::new(kernelPath.to_owned())
+        }
     }
 
+    // Add RAMDSK to the image file
+    pub fn setRamDsk(&mut self, ramdskPath: &Path) -> &mut Self {
+        self.dskImageBuilder.setRamDsk(ramdskPath.to_owned());
+        self
+    }
+
+    // Add a config to manipulate behaviour at runtime
+    pub fn setBootConfig(&mut self, config: &BootConfig) -> &mut Self {
+        self.dskImageBuilder.setBootConfig(config);
+        self
+    }
+
+    // Create the bootable disk image at a specified path
+    pub fn createDskImg(&self, dirPath: &Path) -> anyhow::Result<()> {
+        self.dskImageBuilder.createDskImg(dirPath);
+    }
 }
